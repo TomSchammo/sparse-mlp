ed785a6c075e6c84991aa8d1572176f9437f4a37
Tom Schammo <tom@tomschammo.com>
2026-01-04 15:19:39 +0100

    add custom srelu patch

--- SET-MLP-Keras-Weights-Mask/set_mlp_keras_cifar10.py
+++ SET-MLP-Keras-Weights-Mask/set_mlp_keras_cifar10.py
@@ -47,7 +47,7 @@ from keras import optimizers
 import numpy as np
 from tensorflow.keras import backend as K
 #Please note that in newer versions of keras_contrib you may encounter some import errors. You can find a fix for it on the Internet, or as an alternative you can try other activations functions.
-from keras_contrib.layers.advanced_activations.srelu import SReLU
+from srelu import SReLU
 from keras.datasets import cifar10
 from keras.utils import to_categorical
 
